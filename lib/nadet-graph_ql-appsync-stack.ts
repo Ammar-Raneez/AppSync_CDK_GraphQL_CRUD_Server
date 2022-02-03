a613a74476ccfd827c9376ab22431359abd7b998
ammar <ammarraneez@gmail.com>
2022-02-03 15:59:59 +0530

    fix: issue in graphql schema

--- lib/nadet-graph_ql-appsync-stack.ts
+++ lib/nadet-graph_ql-appsync-stack.ts
@@ -7,16 +7,17 @@ import {
   CfnResolver
 } from 'aws-cdk-lib/aws-appsync';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
-
 export class NadetGraphQlAppSyncStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Api data table
     const notesTable = new Table(this, 'AppSyncNotesTable', {
       tableName: 'AppSyncNotesTable',
       partitionKey: {
@@ -25,6 +26,7 @@ export class NadetGraphQlAppSyncStack extends Stack {
       }
     });
 
+    // Api itself
     const api = new CfnGraphQLApi(this, 'GraphQLAPI', {
       name: 'GraphQLAPI',
       authenticationType: 'API_KEY',
@@ -35,9 +37,41 @@ export class NadetGraphQlAppSyncStack extends Stack {
       apiId: api.attrApiId,
     });
 
+    // api data schema
     const schema = new CfnGraphQLSchema(this, 'GraphQLSchema', {
       apiId: api.attrApiId,
-      definition: require('../graphql/schema.graphql')
+      definition: `
+        schema {
+          query: Query
+          mutation: Mutation
+        }
+
+        type Note {
+          id: ID!
+          name: String!,
+          complete: Boolean!
+        }
+
+        input NoteInput {
+          id: ID!
+          name: String!
+          complete: Boolean!
+        }
+
+        type Query {
+          listNotes: [Note]
+        }
+
+        type Mutation {
+          createNote(note: NoteInput!): Note
+        }
+
+        type Subscription {
+          onCreateNote: Note
+
+          @aws_subscribe(mutations: ["createNote"])
+        }
+      `
     });
 
     new CfnOutput(this, 'GraphQLAPIURL', {
@@ -50,6 +84,20 @@ export class NadetGraphQlAppSyncStack extends Stack {
       exportName: 'GraphQLAPIKey'
     });
 
+    // provide required permissions
+    const appsyncDynamoRole = new Role(this, 'NoteDynamoDBRole', {
+      assumedBy: new ServicePrincipal('appsync.amazonaws.com'),
+    });
+
+    appsyncDynamoRole.addToPolicy(
+      new PolicyStatement({
+        resources: ['*'],
+        actions: ['dynamodb:*', 'lambda:*', 'logs:*', 'cognito-idp:*'],
+        effect: Effect.ALLOW,
+      })
+    );
+
+    // create api lambdas
     const notesLambda = new NodejsFunction(this, 'NotesAPIhandler', {
       functionName: 'NotesAPIhandler',
       runtime: Runtime.NODEJS_14_X,
@@ -57,10 +105,10 @@ export class NadetGraphQlAppSyncStack extends Stack {
       entry: join(__dirname, '..', 'lambdas', 'main.ts'),
       environment: {
         NOTES_TABLE: notesTable.tableName
-      } 
+      }
     });
-    notesTable.grantFullAccess(notesLambda);
 
+    // connect datasource to the api
     const dataSource = new CfnDataSource(this, 'NoteHandlerDataSource', {
       name: 'NoteHandlerDataSource',
       apiId: api.attrApiId,
@@ -68,25 +116,25 @@ export class NadetGraphQlAppSyncStack extends Stack {
       lambdaConfig: {
         lambdaFunctionArn: notesLambda.functionArn
       },
-      dynamoDbConfig: {
-        tableName: 'AppSyncNotesTable',
-        awsRegion: props?.env?.region!
-      }
+      serviceRoleArn: appsyncDynamoRole.roleArn
     });
 
-    // resolvers for subscriptions are automatically created
+    // create api resolvers - resolvers for subscriptions are automatically created
     new CfnResolver(this, 'NoteQueryResolver', {
       apiId: api.attrApiId,
       typeName: 'Query',
       fieldName: 'listNotes',
       dataSourceName: dataSource.name
-    });
+    }).addDependsOn(schema);
 
     new CfnResolver(this, 'NoteMutationResolver', {
       apiId: api.attrApiId,
       typeName: 'Mutation',
       fieldName: 'createNote',
       dataSourceName: dataSource.name
-    });
+    }).addDependsOn(schema);
+
+    // grant lambda full access to the db
+    notesTable.grantFullAccess(notesLambda);
   }
 }
